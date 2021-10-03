14078d668c31491ddbd990daad1737d847763851
rodrigodojo <rodrigodoj@gmail.com>
2021-10-03 19:32:03 -0300

    endpoint simples criado e testado.

--- src/main/java/com/rodrigodojo/projetoaws/controller/testController.java
+++ src/main/java/com/rodrigodojo/projetoaws/controller/testController.java
@@ -1,4 +1,22 @@
 package com.rodrigodojo.projetoaws.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/test")
 public class testController {
+    private static final Logger LOG = LoggerFactory.getLogger(testController.class);
+
+    @GetMapping("/dog/name")
+    public ResponseEntity<?> dogTest(@PathVariable String name){
+        LOG.info("Teste de controler - name : {}", name);
+
+        return ResponseEntity.ok("Name : "+ name);
+    }
 }
